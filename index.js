dcf48fe97802faaaec38dd76ea5f77993c1131a0
MuhammadRiyansyah <mriyansyah112@gmail.com>
2022-09-14 02:54:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let passwordLength = document.getElementById("passwordLength");
+let password = document.getElementById("password");
+
+const getPassword = document.querySelector(".btn-gen");
+const savePassword = document.querySelector(".btn-save");
+
+const generatePassword = (len) => {
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numeric = "0123456789";
+  const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>~`";
+
+  const data = lowerAlphabet + upperAlphabet + numeric + symbol;
+  let generator = "";
+  for (let index = 0; index < len; index++) {
+    generator += data[~~(Math.random() * data.length)];
+  }
+  return generator;
+};
+
+getPassword.addEventListener("click", function () {
+  const newPassword = generatePassword(passwordLength.value);
+  password.value = newPassword;
+});
+
+savePassword.addEventListener("click", function () {
+  document.title = password.value;
+  savePassword.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," +
+      encodeURIComponent(`password saya: ${document.title}`)
+  );
+  savePassword.setAttribute("download", "MyPasswordGeneratorLOG.txt");
+});
+
+function updateTextInput(val) {
+  document.getElementById("value-range").value = val;
+}
