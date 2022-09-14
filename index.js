7ac91da79c7877adb0191de83b814fbbb8478a72
MuhammadRiyansyah <mriyansyah112@gmail.com>
2022-09-14 18:10:00 +0700

    Last

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-let passwordLength = document.getElementById("passwordLength");
-let password = document.getElementById("password");
+const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+const numeric = "0123456789";
+const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>~`";
 
-const getPassword = document.querySelector(".btn-gen");
+// Selector Button
 const savePassword = document.querySelector(".btn-save");
 
-const generatePassword = (len) => {
-  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numeric = "0123456789";
-  const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>~`";
+// Selector Toggle && Input
 
-  const data = lowerAlphabet + upperAlphabet + numeric + symbol;
-  let generator = "";
-  for (let index = 0; index < len; index++) {
-    generator += data[~~(Math.random() * data.length)];
-  }
-  return generator;
+// Input
+const totalChar = document.getElementById("passwordLength");
+const passwordBox = document.getElementById("password");
+// Toggle
+const upperInput = document.querySelector("#Upper-case");
+const lowerInput = document.querySelector("#Lower-case");
+const numberInput = document.querySelector("#Numbers");
+const symbolInput = document.querySelector("#Symbols");
+
+const getRandomData = (dataSet) => {
+  return dataSet[~~(Math.random() * dataSet.length)];
 };
 
-getPassword.addEventListener("click", function () {
-  const newPassword = generatePassword(passwordLength.value);
-  password.value = newPassword;
-});
+const generatePassword = (password = "") => {
+  if (upperInput.checked) {
+    password += getRandomData(upperAlphabet);
+  }
+  if (lowerInput.checked) {
+    password += getRandomData(lowerAlphabet);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(numeric);
+  }
+  if (symbolInput.checked) {
+    password += getRandomData(symbol);
+  }
+  if (password.length < totalChar.value) {
+    return generatePassword(password);
+  }
+  passwordBox.value = truncateString(password, totalChar.value);
+};
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    let subStr = str.substring(0, num);
+    return subStr;
+  } else {
+    return str;
+  }
+}
+
+const getPassword = document
+  .querySelector(".btn-gen")
+  .addEventListener("click", function () {
+    generatePassword();
+  });
 
 savePassword.addEventListener("click", function () {
   savePassword.setAttribute(
     "href",
     "data:text/plain;charset=utf-8," +
-      encodeURIComponent(`password saya: ${password.value}`)
+      encodeURIComponent(`password saya: ${passwordBox.value}`)
   );
   savePassword.setAttribute("download", "MyPasswordGeneratorLOG.txt");
 });
